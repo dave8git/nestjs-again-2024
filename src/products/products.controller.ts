edaca9474752de061368aedd5afc5e603bcef80b
dave8git <dave8git@gmail.com>
2024-12-26 01:40:31 +0100

    Add db data and endpoints for orders added

---
+++ src/products/products.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Param, Delete } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+    constructor(private productsService: ProductsService) {
+        this.productsService = productsService;
+    }
+
+    @Get('/') 
+    getAll(): any {
+        return this.productsService.getAll();
+    }
+    @Get('/:id')
+    public getById(@Param('id') id: string) {
+        return this.productsService.getById(id);
+    }
+    @Delete('/:id')
+    public deleteById(@Param('id') id: string) {
+        this.productsService.deleteById(id);
+        return { success: true};
+    }
+}
